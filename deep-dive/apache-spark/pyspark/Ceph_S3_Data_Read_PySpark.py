02193ef39e1463724b2df1cccd9210acbbc9f9e3
Prateek Dubey <dataengineeringe2e@gmail.com>
2021-09-23 18:02:21 +0800

    Ceph S3 Data Read job

--- deep-dive/apache-spark/pyspark/Ceph_S3_Data_Read_PySpark.py
+++ deep-dive/apache-spark/pyspark/Ceph_S3_Data_Read_PySpark.py
@@ -8,12 +8,12 @@ spark = SparkSession.builder \
         .appName('amazon-data-review') \
         .getOrCreate()
 
-# Read Source Datasets 
+# Read Source Datasets
 
-musical_data= spark.read.json("s3a://dataengineeringe2e/dataset/Musical_Instruments_5.json")
-musical_metadata= spark.read.json("s3a://dataengineeringe2e/dataset/meta_Musical_Instruments.json")
+musical_data= spark.read.json("s3a://spark-demo/Musical_Instruments_data.json")
+musical_metadata= spark.read.json("s3a://spark-demo/Musical_Instruments_metadata.json")
 
-# Register dataframes as temp tables 
+# Register dataframes as temp tables
 
 musical_metadata.registerTempTable("musical_metadata")
 musical_data.registerTempTable("musical_data")
@@ -21,8 +21,8 @@ musical_data.registerTempTable("musical_data")
 # Top products based on unique user reviews
 
 top_rated = spark.sql("""
-select musical_data.asin as product_id, 
-        count(distinct musical_data.reviewerID) as unique_reviewer_id_count, 
+select musical_data.asin as product_id,
+        count(distinct musical_data.reviewerID) as unique_reviewer_id_count,
         musical_metadata.price as product_price
 from musical_data left outer join musical_metadata
 on musical_data.asin == musical_metadata.asin
@@ -40,8 +40,8 @@ top_rated.show(truncate=False)
 top_rated.write.format("csv") \
         .option("header","true") \
         .mode("overwrite") \
-        .save("s3a://dataengineeringe2e/output/amazon_data_review/top_10_rated")
+        .save("s3a://spark-demo/output/amazon_data_review/top_10_rated")
 
-# Stop Spark Context to release resources 
+# Stop Spark Context to release resources
 
 spark.stop()
